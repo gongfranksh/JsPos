7dc247acf3bbc1b0f319ad5ebdb7294378732ab4
gongfranksh <gongfranksh@qq.com>
2018-10-23 10:21:13 +0800

    +设置自动更新activity的动作

--- app/src/main/java/personal/wl/jspos/UpgradeActivity.java
+++ app/src/main/java/personal/wl/jspos/UpgradeActivity.java
@@ -1,13 +1,30 @@
 package personal.wl.jspos;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import personal.wl.jspos.method.PosTabInfo;
+import personal.wl.jspos.update.http.HttpToolsKits;
 
 public class UpgradeActivity extends AppCompatActivity {
+    private TextView upgrade_readme;
+    private String displaystr;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_upgrade);
+        PosTabInfo posTabInfo = new PosTabInfo(UpgradeActivity.this);
+        displaystr = "当前系统版本为:" + posTabInfo.getPackageName();
+        upgrade_readme = findViewById(R.id.upgrade_readme);
+        upgrade_readme.setText(displaystr);
+//        UpgradeUI ugui = new UpgradeUI(UpgradeActivity.this);
+//        ugui.getversion();
+
+        HttpToolsKits httpToolsKits = new HttpToolsKits(UpgradeActivity.this, getWindow().getDecorView());
+        httpToolsKits.downloadVersionFile();
+
+
     }
 }
