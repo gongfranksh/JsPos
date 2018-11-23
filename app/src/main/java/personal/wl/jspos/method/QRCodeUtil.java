56ccda73928453bfefa1fe901228012c55d83118
gongfranksh <gongfranksh@qq.com>
2018-11-23 13:20:52 +0800

    修改了发票服务地址

--- app/src/main/java/personal/wl/jspos/method/QRCodeUtil.java
+++ app/src/main/java/personal/wl/jspos/method/QRCodeUtil.java
@@ -21,7 +21,7 @@ import static personal.wl.jspos.method.DeviceUtils.ToolsGetSaleAmt;
 public class QRCodeUtil {
 
 
-    public static String QR_PREX = "http://tfp.buynow.com.cn:8090/invoice/qr?saleid=saleid&braid=braid&saledate=saledate";
+    public static String QR_PREX = "http://fapiao.buynow.com.cn:8090/invoice/qr?saleid=saleid&braid=braid&saledate=saledate";
 
     public static Bitmap createQRCodeBitmap(String content, int width, int height,
                                             String character_set, String error_correction_level,
